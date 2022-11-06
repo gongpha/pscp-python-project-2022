619e10ac699de106868d1d800707379d36af82a8
Kongfa Waroros <gongpha@hotmail.com>
2022-11-06 20:11:39 +0700

    add more dialogues

--- script/dialogue.py
+++ script/dialogue.py
@@ -1,3 +1,8 @@
 """dialogue storage"""
 
 order = ["Can I have {}x {}, please"]
+order_ok = ["Thanks!"]
+order_not_complete = ["The order is not complete!"]
+order_too_many_items = ["Perhaps you should clean your counter."]
+order_too_long = ["I don't have time for this!"]
+repeat_too_much = ["Why are you keeping me repeating?"]
