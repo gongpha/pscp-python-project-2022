63fb15882d691bfd6abf5bcd88c1f74648d81be1
Kongfa Waroros <gongpha@hotmail.com>
2022-11-07 22:25:14 +0700

    make animation playing

--- script/game.py
+++ script/game.py
@@ -10,6 +10,20 @@ class Game(Control):
     worldspawn : Worldspawn
     rng : RandomNumberGenerator # The number generator for randomizing the item spawn points and more
 
+    ani : AnimationPlayer # Play animations that defined inside the editor
+
+    motd : ColorRect # MOTD (Message of the day) Black screen
+    endday : ColorRect # End day black transparent screen
+    endday_day : Label # The current day text
+    motd_day : Label # The current day in MOTD (THE BIG TEXT)
+
+    hint_day : Label # Day text
+    balance_text : Label # Balance Text (used $ as a placeholder)
+
+    tscale : float = 0.05 # Time scale
+    current_day : int = 1
+    balance : int = 200
+
     def _ready(self):
         # Get the nodes
         self.worldspawn = self.get_node("worldspawn")
@@ -20,10 +34,13 @@ class Game(Control):
         self.endday_day = self.get_node("endday/endday/vbox/day")
         self.motd_day = self.get_node("motd/day")
 
+        self.hint_day = self.get_node("ui/vbox/day")
+        self.balance_text = self.get_node("ui/vbox/balance")
+
 
         # Then, let's initialize the random number generator
         # Random its seed by the current time
-        self.rng = RandomNumberGenerator.new()
+        self.rng = RandomNumberGenerator()
         self.rng.randomize()
 
         # Now, LET'S GOOOOOOOOOOOOOOOOO
@@ -31,5 +48,10 @@ class Game(Control):
 
     def newday(self) :
         """ Called when a game day starts """
-        pass
-        
+
+        # Show MOTD screen
+        self.motd_day.text = "Day %d" % self.current_day
+        self.hint_day.text = self.motd_day.text
+        self.endday.hide()
+        self.ani.play("motd")
+        # Wait for the animation to finish
