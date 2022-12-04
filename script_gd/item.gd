01125da6155859cb8a41e5dbd16405ab928a43fd
jollux_ <gunkunlapat@gmail.com>
2022-12-04 19:41:30 +0700

    updated stall

--- script_gd/item.gd
+++ script_gd/item.gd
@@ -12,12 +12,12 @@ var prev_point_quat : Quat
 var home_place : Vector3
 
 func _ready() :
-	PYPORT.call("item_ready", self)
+    PYPORT.call("item_ready", self)
 
 func _integrate_forces(state : PhysicsDirectBodyState) :
-	# physics thing
-	# ask gongpha for the questions
-	if !get_picking :
-		return
-	var vel := get_picking.global_translation - global_translation
-	state.set_linear_velocity(vel * 20)
+    # physics thing
+    # ask gongpha for the questions
+    if !get_picking :
+        return
+    var vel := get_picking.global_translation - global_translation
+    state.set_linear_velocity(vel * 20)
